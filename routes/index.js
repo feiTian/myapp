e64b99ff5809e374e50368a31fb29b104a14c209
Fei <hero_afei@163.com>
2016-01-20 15:23:24 +0800

    add carInfo api

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+    res.render('index', { title: 'Express' });
 });
+
+router.post('/carInfo', writeCarInfo);
+
+function writeCarInfo(req, res){
+    console.log('getting car info.');
+    console.log(req.body);
+
+	console.log("get school successful");
+	res.contentType('json');
+	res.send(JSON.stringify({ rtn: 0, message:"ok" }));
+	res.end();
+}
 
 module.exports = router;
