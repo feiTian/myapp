9efb8c6ccda032702f7342b46d4138b4eea06e68
Fei <hero_afei@163.com>
2016-01-22 09:59:35 +0800

    change pw

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var connection = mysql.createConnection({
 //	host     : 'localhost',
     host: 'localhost',
     user: 'root',
-    password: 'heroafei',
+    password: '',
     database: 'test'
 });
 
